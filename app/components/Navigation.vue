01aceae17b44aea6520b821ed380b4aeb770ebc7
Meccado <tsw603gp@gmail.com>
2026-02-20 18:41:44 +0200

    fix: navigation menu - cart badge access with computed values

--- app/components/Navigation.vue
+++ app/components/Navigation.vue
@@ -47,10 +47,10 @@
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
             </svg>
             <span
-              v-if="cartStore.itemCount > 0"
+              v-if="(cartStore.itemCount.value || cartStore.itemCount) > 0"
               class="absolute -top-2 -right-2 bg-gold text-luxury-green rounded-full w-5 h-5 flex items-center justify-center text-xs font-bold animate-pop-in dark:bg-luxury-green dark:text-white"
             >
-              {{ cartStore.itemCount }}
+              {{ cartStore.itemCount.value || cartStore.itemCount }}
             </span>
           </NuxtLink>
           
